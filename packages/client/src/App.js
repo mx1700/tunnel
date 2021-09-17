b249f7c3a660e7a83ae028e2f5f1f33c28d15b8f
li.zhaoguang <li.zhaoguang@immomo.com>
2021-09-17 21:09:19 +0800

    样式调整

--- packages/client/src/App.js
+++ packages/client/src/App.js
@@ -7,13 +7,14 @@ import {
     StyledOcticon,
     ThemeProvider,
     theme,
-    TextInput, FilteredSearch, Dropdown, SubNav, BaseStyles, ButtonPrimary, UnderlineNav, Heading
+    TextInput, FilteredSearch, Dropdown, SubNav, BaseStyles, ButtonPrimary, UnderlineNav, Heading, ButtonDanger, Spinner
 } from '@primer/components'
 import deepmerge from 'deepmerge'
 import ReactJson from "react-json-view";
 import {RequestTable} from "./components/RequestTable";
 import {RequestDetail} from "./components/RequestDetail";
 import {LogData} from "./data";
+import {useState} from "react";
 
 // const customTheme = deepmerge(theme, {
 //     fonts: {
@@ -22,6 +23,7 @@ import {LogData} from "./data";
 // })
 
 function App() {
+    const [connected, setConnected] = useState(false)
   return (
       <ThemeProvider>
           <BaseStyles>
@@ -38,14 +40,16 @@ function App() {
             </Header.Item>
           </Header>
           <Box m={4} p={4} pt={3} bg="bg.primary">
-              <SubNav aria-label="Main" mb={3}>
+              <SubNav aria-label="Main" mb={3} display="flex">
                   <FilteredSearch>
                       <Dropdown>
                           <Dropdown.Button>手机号</Dropdown.Button>
                       </Dropdown>
-                      <TextInput type="search" width={160} />
+                      <TextInput type="search" disabled={connected} width={160} />
                   </FilteredSearch>
-                  <ButtonPrimary ml={2}>开始</ButtonPrimary>
+                  { !connected && <ButtonPrimary ml={2} onClick={() => setConnected(true)} >开始</ButtonPrimary> }
+                  { connected && <><ButtonDanger ml={2} onClick={() => setConnected(false)}>停止</ButtonDanger><Spinner size="medium"/></> }
+                  <Button ml={2}>清屏</Button>
               </SubNav>
               <Box display="flex">
                   <Box flexGrow={1} mr={3}>
