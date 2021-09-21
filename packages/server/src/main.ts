c702bc13829f634cf9fee457155c37c033a5dfb4
li.zhaoguang <li.zhaoguang@immomo.com>
2021-09-22 00:00:26 +0800

    完成数据存储

--- packages/server/src/main.ts
+++ packages/server/src/main.ts
@@ -1,9 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { PrismaService } from './prisma.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
+  const prismaService: PrismaService = app.get(PrismaService);
+  prismaService.enableShutdownHooks(app);
   await app.listen(process.env.PORT || 3001);
 }
 bootstrap();
