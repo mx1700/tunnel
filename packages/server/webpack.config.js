5db4b9fd1c4dd7ccd4c3f0ed09420e9ad25740b9
lizhaoguang <mx1700@gmail.com>
2021-09-23 19:14:54 +0800

    prisma build 支持

--- packages/server/webpack.config.js
+++ packages/server/webpack.config.js
@@ -1,3 +1,6 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const CopyPlugin = require('copy-webpack-plugin');
+
 module.exports = (options, webpack) => {
   const lazyImports = [
     '@nestjs/microservices/microservices-module',
@@ -6,7 +9,9 @@ module.exports = (options, webpack) => {
 
   return {
     ...options,
-    externals: [],
+    externals: {
+      _http_common: 'commonjs2 _http_common',
+    },
     plugins: [
       ...options.plugins,
       new webpack.IgnorePlugin({
@@ -20,6 +25,17 @@ module.exports = (options, webpack) => {
           }
           return false;
         },
+      }),
+      new CopyPlugin({
+        patterns: [
+          { from: 'prisma/schema.prisma', to: '.' },
+          { from: 'prisma/tunnel.db', to: '.' },
+          {
+            from: 'node_modules/.prisma/client/libquery_engine-darwin-arm64.dylib.node',
+            to: '.',
+          },
+          // { from: 'other', to: 'public' },
+        ],
       }),
     ],
   };
